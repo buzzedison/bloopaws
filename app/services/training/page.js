a261886876811488e2fc6b399f0aec5ec7fd29b0
Edison <buzzedison@gmail.com>
2023-10-10 11:38:35 -0400

    added training services

---
+++ app/services/training/page.js
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function TrainingServices() {
+  const services = [
+    'Management and Leadership',
+    'Business Strategy',
+    'Fundraising Techniques',
+    'Marketing Strategies',
+    'Customer Service Excellence',
+    'Technology Updates and Skills',
+    'Team Building and Collaboration'
+  ];
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+      {/* Hero Section */}
+      <section className="relative h-[600px] flex items-center justify-center text-center bg-red-800">
+        {/* <Image src="/images/training-hero.jpg" layout="fill" objectFit="cover" alt="Training" className="absolute z-0" /> */}
+        <div className="relative z-10">
+          <h1 className="text-5xl font-extrabold text-white px-2 md:px-24">Training</h1>
+          <p className="mt-4 text-lg text-white max-w-2xl mx-auto">We deliver exceptional training solutions tailored to your unique needs.</p>
+        </div>
+      </section>
+
+      {/* Services Subsection */}
+      <section id="services" className="container mx-auto my-16 p-10">
+        <h2 className="text-3xl font-semibold text-center mb-8">Our Training Programs</h2>
+        <ul className="text-gray-700 text-center grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+          {services.map((service, i) => (
+            <li key={i} className="p-6 bg-white rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300 ease-in-out hover:bg-red-100">
+              {service}
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      {/* CTA Section */}
+      <section className="bg-red-700 text-white p-16 text-center">
+        <h2 className="text-4xl font-bold mb-4">Ready to Enhance Your Skills?</h2>
+        <p className="text-lg mb-8">Contact us today to find the perfect training program for you and your team.</p>
+        <Link href="/contact">
+          <button className="bg-white text-red-700 px-8 py-2 rounded-full text-lg font-semibold hover:bg-gray-300">
+            Learn More
+          </button>
+        </Link>
+      </section>
+    </div>
+  );
+}
