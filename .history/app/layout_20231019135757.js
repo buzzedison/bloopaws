186affc4ec8ccfbe106dd70a2852e7ed1756966c
Edison <buzzedison@gmail.com>
2023-10-19 14:04:09 -0400

    google analytics added

--- .history/app/layout_20231019135757.js
+++ .history/app/layout_20231019135757.js
@@ -3,7 +3,7 @@ import { Poppins } from 'next/font/google'
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer";
 import TidioChat from './components/TidioChat';
-import GoogleAnalytics from "../app/GoogleAnalytics";
+import GoogleAnalytics from "~/components/GoogleAnalytics";
 const pop = Poppins({ 
   subsets: ['latin'] ,
   weight:["400", "500", "600"]
