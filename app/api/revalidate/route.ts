3cf8b5de21fc76dc7326e621a55af4ea97cf26a9
Edison <buzzedison@gmail.com>
2024-11-21 09:47:26 -0500

    added revalidate

---
+++ app/api/revalidate/route.ts
@@ -0,0 +1,32 @@
+import { revalidateTag } from 'next/cache'
+import { type NextRequest, NextResponse } from 'next/server'
+import { parseBody } from 'next-sanity/webhook'
+
+export async function POST(req: NextRequest) {
+  try {
+    const { body, isValidSignature } = await parseBody<{
+      _type: string;
+      slug?: string;
+    }>(req, process.env.SANITY_WEBHOOK_SECRET, true) // Added delay parameter
+
+    if (!isValidSignature) {
+      return new Response('Invalid signature', { status: 401 })
+    }
+
+    if (!body?._type) {
+      return new Response('Bad Request', { status: 400 })
+    }
+
+    // Revalidate the content
+    revalidateTag(body._type)
+
+    return NextResponse.json({
+      status: 200,
+      revalidated: true,
+      now: Date.now(),
+      body
+    })
+  } catch (error: any) {
+    return new Response(error.message, { status: 500 })
+  }
+}
